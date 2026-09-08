b014f2cad462edda98f96810a4ac03aea34d3951
Juan-Francisco-Martin-Saez <reijohan@gmail.com>
2026-09-08 15:13:42 +0200

    Pruebas de JS iniciales

---
+++ GPT-JS/scr/menu.js
@@ -0,0 +1,9 @@
+export default (() => {
+  alert('Hola')
+  alert('¿Cómo lo llevamos?')
+  alert('Espero que bien')
+  alert('¿Preparado para empezar?')
+  alert('¡Venga, a tope!')
+  alert('¡Vamos allá!')
+  alert('¿Empezamos?')
+})()
